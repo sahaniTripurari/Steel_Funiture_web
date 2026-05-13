6cd6e66edfd354f74d3ca63565ef38a8afa90ccd
sahniTrirpurari <sahanitrirpurari@gmail.com>
2026-05-13 05:45:43 +0530

    fix: update wildcard route for Express 5 compatibility

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/messages', messagesRouter);
 // Serve Static Files for Production
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   if (!req.path.startsWith('/api/')) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   }
